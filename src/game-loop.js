abaa4c1f9fa3ed0077fa1eca22a6c20f7b7182ec
Raye <rayemayberry@yahoo.com>
2019-04-03 17:16:21 -0500

    Revert "refactor game loop and update birdfeeder functionality to add birds at faster intervals rather than add more birds per second"
    This reverts commit 24bbcd91e0430f18acd0d8ea0a83973c9646bcb1.

--- src/game-loop.js
+++ src/game-loop.js
@@ -1,6 +1,7 @@
 import render from '../index.js';
 
-function leaveTrinkets(state){
+export default function (state){
+    // every 1 second a bird leaves
     setInterval(()=>{
         if(state.Resources.birds >= 1){
         state.Resources.birds --;
@@ -10,20 +11,9 @@ function leaveTrinkets(state){
         render(state);
 
     }, 1000); 
-}
-
-function attractBirds(state){
-    let ratio = state.Resources.birds + 1;
-
-    setInterval(()=>{
-        state.Resources.birds ++;
-        render(state);
-    }, 10000 / ratio);
-}
-
-export default function (state){
-    // every 1 second a bird leaves
-    leaveTrinkets(state);
     // birdfeeders attract birds without clicking
-    attractBirds(state);
+    setInterval(()=>{
+        state.Resources.birds += state.Resources.birdfeeders;
+        render(state);
+    }, 10000);
 }
