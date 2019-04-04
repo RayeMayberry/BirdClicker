76ff940c7459b85046ffb00fdfe644857abbb38b
Raye <rayemayberry@yahoo.com>
2019-04-03 21:20:35 -0500

    add accurate button messages

--- state/buttons.js
+++ state/buttons.js
@@ -23,7 +23,7 @@ class button {
             State.Resources[this.buy] ++;
         }
         else{
-            State.Messages.push(`Not enough ${this.cost}`)
+            State.Messages.push(`Not enough ${this.cost}: (${State.Resources[this.cost]}/${this.ratio})`)
         }
     }
 }
