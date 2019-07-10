4c9c3d877f6cfb7420e3ed3fd6594ac50773781f
Raye <rayemayberry@yahoo.com>
2019-07-10 10:34:36 -0500

    add bassic background layer

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import Phaser from 'phaser';
+import Overworld from './assets/overworld_tileset_grass.png';
 
 var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    parent: 'root',
     scene: {
         preload: preload,
         create: create,
@@ -15,12 +17,28 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
+    // assets go here!
+    this.load.image('overworld', Overworld);
+
 }
 
 function create ()
 {
+    // code for bg tiles
+    const garden = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
+    ];
+    const map = this.make.tilemap({ data: garden, tileWidth: 16, tileHeight: 16 });
+    const tiles = map.addTilesetImage("overworld");
+    const layer = map.createStaticLayer(0, tiles, 0, 0);
 }
 
-function update ()
+function update (time, delta)
 {
 }
